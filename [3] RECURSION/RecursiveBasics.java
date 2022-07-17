1e1c6fabc1a5ffc49bc55ea2534f3c96a4242888
AnushaDeviR <anusha3599@gmail.com>
2022-07-17 17:52:41 +0700

    recursive basics

---
+++ [3] RECURSION/RecursiveBasics.java
@@ -0,0 +1,10 @@
+class RecursiveBasics {
+    static void fun1() {
+        System.out.println("fun1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Before fun1");
+        fun1();
+    }
+}
